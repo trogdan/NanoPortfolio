afa65421c3a8d8cccc5edbb07536f6d3b48c62a2
trogdan <mr.trogdan@gmail.com>
2015-06-10 22:29:30 -0400

    add drawables for button states

--- app/src/main/java/com/trogdan/nanoportfolio/MainActivity.java
+++ app/src/main/java/com/trogdan/nanoportfolio/MainActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -12,12 +14,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button spotifyButton = (Button) findViewById(R.id.spotifyButton);
+        spotifyButton.setOnClickListener(
+                new Button.OnClickListener() {
+                    public void onClick(View v) {
+
+                    }
+                });
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+
         return true;
     }
 
