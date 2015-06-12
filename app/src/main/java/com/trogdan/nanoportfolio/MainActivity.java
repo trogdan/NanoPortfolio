1a53f75569ce2c7c586c7de01eae467d99ed45d5
trogdan <mr.trogdan@gmail.com>
2015-06-11 23:02:58 -0400

    Add material design support library and snackbar demo

--- app/src/main/java/com/trogdan/nanoportfolio/MainActivity.java
+++ app/src/main/java/com/trogdan/nanoportfolio/MainActivity.java
@@ -1,6 +1,7 @@
 package com.trogdan.nanoportfolio;
 
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -10,10 +11,40 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private final View.OnClickListener defaultSnackbarListener = new View.OnClickListener() {
+        public void onClick(View v) {
+            //TODO
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final View coordinatorLayoutView = findViewById(R.id.snackbarPosition);
+        final Button[] buttons = {
+                (Button) findViewById(R.id.spotifyButton),
+                (Button) findViewById(R.id.scoresButton),
+                (Button) findViewById(R.id.libraryButton),
+                (Button) findViewById(R.id.biggerButton),
+                (Button) findViewById(R.id.xyzButton),
+                (Button) findViewById(R.id.capstoneButton)
+        };
+
+        for(int i = 0; i < buttons.length; i++)
+        {
+            buttons[i].setOnClickListener(
+                    new Button.OnClickListener() {
+                        public void onClick(View v) {
+                            Snackbar
+                                    .make(coordinatorLayoutView, R.string.snackbar_text, Snackbar.LENGTH_LONG)
+                                    .setAction(R.string.snackbar_action_dismiss, defaultSnackbarListener)
+                                    .setActionTextColor(getResources().getColor(R.color.amber_300))
+                                    .show();
+                        }
+                    });
+        }
     }
 
     @Override
